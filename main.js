ebad90a19b1231c0696bde084936ce80989377fe
Ryo Ota <nwtgck@gmail.com>
2017-12-10 21:39:29 +0900

    [change] Add --output-file options and Save svg to file or output stdout

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 // (from: http://cartman0.hatenablog.com/entry/2017/05/14/172311)
 
 const fs       = require('fs');
+const fse      = require('fs-extra');
+const util     = require('util');
 const process  = require('process');
 const getStdin = require('get-stdin');
 const mjAPI    = require('mathjax-node');
+const program  = require('commander');
+
+program
+    .option('-o --output-file [FILE]', "Output file name")
+    .parse(process.argv);
 
 mjAPI.config({
     mjAPI: {
@@ -12,17 +19,39 @@ mjAPI.config({
 })
 mjAPI.start();
 
-getStdin().then(str => {
-    const yourMath = str;
-    
-    mjAPI.typeset({
-        math: yourMath,
+function isInputFileSpecified(){
+    return program.args.length === 1;
+}
+
+(async function(){
+    let mathStr = null;
+    if(isInputFileSpecified()){
+        const filePath = program.args[0];
+        const exists   = await fse.pathExists(filePath);
+        if(exists){
+            mathStr = await fse.readFile(program.args[0]);            
+        } else {
+            console.error(`Error: '${filePath}' not found`);
+            process.exit(1);
+        }
+    } else {
+        mathStr = await getStdin();        
+    }
+    const data    = await mjAPI.typeset({
+        math: mathStr,
         format: "TeX", // "inline-TeX", "MathML"
         svg: true,
-      }, function (data) {
-        if (!data.errors) {
-            fs.writeFileSync("math.svg", data.svg);
-        }
-      });
-});
+      }
+    );
+    if(!isInputFileSpecified() && program.outputFile == undefined){
+        // Output to stdout
+        console.log(data.svg);
+    } else {
+        // Decide file path
+        const outFilePath =  program.outputFile || `${program.args[0]}.svg`
+        // Write to a file
+        await fse.writeFile(outFilePath, data.svg);
+        console.log(`Saved to ${outFilePath}`);
+    }   
+})();
 
